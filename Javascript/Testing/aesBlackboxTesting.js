d45ec05b03902434a50b4646991b40d874b8a1f1
Lena Voytek <dvoytek@email.arizona.edu>
2019-11-01 15:22:16 -0700

    Blackbox testing with any AES type

--- Javascript/Testing/aesBlackboxTesting.js
+++ Javascript/Testing/aesBlackboxTesting.js
@@ -34,10 +34,25 @@ document.addEventListener("DOMContentLoaded", function(event)
 	document.getElementById("process").addEventListener("click", function()	
 	{
 		if(/^([a-fA-F0-9]{2})+$/.test(keyInput.value))
-		{
-			let encrypted = AES128.encrypt(plaintextInput, getStringAsHexArr(keyInput.value));
-			encryptedOutput.value = encrypted;
-			plaintextOutput = AES128.decrypt(encrypted, getStringAsHexArr(keyInput.value));
+		{	
+			if(document.getElementById("doAES128").checked)
+			{
+				let encrypted = AES128.encrypt(plaintextInput.value, getStringAsHexArr(keyInput.value));
+				encryptedOutput.value = getHexArrayAsString(encrypted);
+				plaintextOutput.value = AES128.decrypt(encrypted, getStringAsHexArr(keyInput.value));
+			}
+			else if(document.getElementById("doAES192").checked)
+			{
+				let encrypted = AES192.encrypt(plaintextInput.value, getStringAsHexArr(keyInput.value));
+				encryptedOutput.value = getHexArrayAsString(encrypted);
+				plaintextOutput.value = AES192.decrypt(encrypted, getStringAsHexArr(keyInput.value));
+			}
+			else
+			{
+				let encrypted = AES256.encrypt(plaintextInput.value, getStringAsHexArr(keyInput.value));
+				encryptedOutput.value = getHexArrayAsString(encrypted);
+				plaintextOutput.value = AES256.decrypt(encrypted, getStringAsHexArr(keyInput.value));
+			}
 		}
 	});
 
