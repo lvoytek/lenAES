1f336afd5f3ab669c5cf6097ab34c9726978b808
Lena Voytek <dvoytek@email.arizona.edu>
2019-11-01 13:10:43 -0700

    decrypt

--- Javascript/Testing/aesBlackboxTesting.js
+++ Javascript/Testing/aesBlackboxTesting.js
@@ -4,6 +4,21 @@ var keyInput = document.getElementById("hexkeystring");
 var encryptedOutput = document.getElementById("encryptedhexstring");
 var plaintextOutput = document.getElementById("plaintextresult");
 
+function getStringAsHexArr(hexString)
+{
+	if(hexString.length % 2 != 0)
+		hexString += '0';
+
+	let hexStringArr = hexString.split("");
+
+	let hexarr = [];
+
+	for(let i = 0; i < hexStringArr.length; i += 2)
+		hexarr.push((hexCharToInt(hexStringArr[i]) << 4) + (hexCharToInt(hexStringArr[i + 1])));
+
+	return hexarr;
+}
+
 
 document.addEventListener("DOMContentLoaded", function(event)
 {
@@ -20,7 +35,9 @@ document.addEventListener("DOMContentLoaded", function(event)
 	{
 		if(/^([a-fA-F0-9]{2})+$/.test(keyInput.value))
 		{
-			
+			let encrypted = AES128.encrypt(plaintextInput, getStringAsHexArr(keyInput.value));
+			encryptedOutput.value = encrypted;
+			plaintextOutput = AES128.decrypt(encrypted, getStringAsHexArr(keyInput.value));
 		}
 	});
 
