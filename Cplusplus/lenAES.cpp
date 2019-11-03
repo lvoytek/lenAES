208da622c7840cd16fa7587a3bac5009f7909ab1
Lena Voytek <dvoytek@email.arizona.edu>
2019-11-03 15:52:27 -0700

    Added cpp file skeleton

---
+++ Cplusplus/lenAES.cpp
@@ -0,0 +1,111 @@
+#include "lenAES.h"
+
+void AES::addRoundKey(unsigned char * state, int * word)
+{
+
+}
+
+void AES::subBytes(unsigned char * state)
+{
+
+}
+
+void AES::invSubBytes(unsigned char * state)
+{
+
+}
+
+void AES::shiftRows(unsigned char * state)
+{
+
+}
+
+void AES::invShiftRows(unsigned char * state)
+{
+
+}
+
+void AES::mixColumns(unsigned char * state)
+{
+
+}
+
+void AES::invMixColumns(unsigned char * state)
+{
+
+}
+
+unsigned char AES::mul(unsigned char s1, unsigned char scalar)
+{
+	return 0x0;
+}
+
+unsigned int AES::subWord(unsigned int w)
+{
+	return 0x0;
+}
+
+unsigned int AES::rotWord(unsigned int w)
+{
+	return 0x0;
+}
+
+unsigned int AES::Rcon(unsigned char i)
+{
+	return 0x0;
+}
+
+void AES::cipher(unsigned char * inArr, unsigned int * wArr)
+{
+
+}
+
+void AES::invCipher(unsigned char * inArr, unsigned int * wArr)
+{
+
+}
+
+void AES::keyExpansion(unsigned char * expandedKey, unsigned char * key)
+{
+
+}
+
+void AES::generalEncrypt(unsigned char * output, char * plaintext, unsigned char * key)
+{
+
+}
+
+void AES::generalEncrypt(char * plaintext, unsigned char * input, unsigned char * key)
+{
+
+}
+
+void AES::aes128Encrypt(unsigned char * output, char * plaintext, unsigned char * key)
+{
+
+}
+
+void AES::aes192Encrypt(unsigned char * output, char * plaintext, unsigned char * key)
+{
+
+}
+
+void AES::aes256Encrypt(unsigned char * output, char * plaintext, unsigned char * key)
+{
+
+}
+
+void AES::aes128Decrypt(char * plaintext, unsigned char * input, unsigned char * key)
+{
+
+}
+
+void AES::aes192Decrypt(char * plaintext, unsigned char * input, unsigned char * key)
+{
+
+}
+
+void AES::aes256Decrypt(char * plaintext, unsigned char * input, unsigned char * key)
+{
+
+}
